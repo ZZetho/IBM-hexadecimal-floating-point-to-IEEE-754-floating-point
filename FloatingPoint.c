ac14c4b9e5723dc3c369c9ced17e3ac00d06ae75
wilia181 <wilia181@student.otago.ac.nz>
2025-04-17 16:05:57 +1200

    done perhaps

--- FloatingPoint.c
+++ FloatingPoint.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 #include <stdint.h>
 
+/*
 void printIEEEsingle(uint32_t ieeeSingle)
 {
     float * out = &ieeeSingle;
 
-    printf("%.8f\n", *out);
+    printf("%.80f\n", *out);
 }
 
 void printIEEEdouble(uint64_t ieeeDouble)
 {
     double * out = &ieeeDouble;
 
-    printf("%.16lf\n", *out);
+    printf("%.160lf\n", *out);
 }
+*/
 
 int main(int argc, char * argv[])
 {
     char inputFilename[1024];
     char outputFilename[1024];
 
-    FILE * inputFile = fopen("input.txt", "rb");
-    FILE * outputFile = fopen("output.txt", "wb");
+    FILE * inputFile;
+    FILE * outputFile;
 
     char inputPrecisionChar;
     char outputPrecisionChar;
@@ -29,10 +31,10 @@ int main(int argc, char * argv[])
     const uint8_t SINGLE = 1;
     const uint8_t DOUBLE = 2;
 
-    uint8_t inputPrecision = SINGLE;
-    uint8_t outputPrecision = SINGLE;
+    uint8_t inputPrecision;
+    uint8_t outputPrecision;
 
-    /*
+
     // get the input filename from the user and its precision
     printf("Enter input filename: ");
     if (fscanf(stdin, " %1023[^\n]", inputFilename) != 1)
@@ -104,7 +106,7 @@ int main(int argc, char * argv[])
         return 1;
     }
     //printf("opened: %s\n", outputFilename);
-    */
+
 
     if (inputPrecision == DOUBLE && outputPrecision == DOUBLE)
     {
@@ -113,8 +115,11 @@ int main(int argc, char * argv[])
         // read eight bytes into the buffer
         while (fread(inputBuffer, sizeof(uint8_t) * 8, 1, inputFile) == 1)
         {
+            // 1 bit
             uint8_t sign = (inputBuffer[0] & 0x80) >> 7;
+            // 7 bits
             uint16_t IBMexponent = (inputBuffer[0] & 0x7F);
+            // 56 bits
             uint64_t fraction = ((uint64_t)inputBuffer[1] << 48) | ((uint64_t)inputBuffer[2] << 40) | (uint64_t)inputBuffer[3] << 32 | (uint64_t)inputBuffer[4] << 24 | (uint64_t)inputBuffer[5] << 16 | (uint64_t)inputBuffer[6] << 8 | (uint64_t)inputBuffer[7];
 
             //printf("fraction: %llx\n", fraction);
@@ -126,8 +131,8 @@ int main(int argc, char * argv[])
             for (oneIndex = 0; oneIndex < 65; oneIndex++) {
                 if (oneIndex == 64)
                 {
-                    // TODO no ones found, = 0
-                    printf("todo\n");
+                    // zero
+                    oneIndex = 56;
                     break;
                 }
                 if (fraction >> oneIndex == 1)
@@ -139,18 +144,37 @@ int main(int argc, char * argv[])
             //printf("%d\n", oneIndex);
 
             // move the fraction left by oneIndex for the implicit 1 of IEEE and cut off the first one
-            fraction = (fraction << oneIndex) & 0xFFFFFFFFFFFFFF;
+            fraction = ((fraction << oneIndex) & 0xFFFFFFFFFFFFFF) >> 4;
 
             // new exponent (number of bits the fraction should be moved)
             // the number of bits IBM moved it
             // minus the index of the first one as IEEE has an implicit one
             // IEEE double is biased by 1023
-            uint16_t IEEEexponent = ((IBMexponent - 64) * 4) - oneIndex + 1023;
+            int32_t IEEEexponent = ((IBMexponent - 64) * 4) - oneIndex + 1023;
 
             uint64_t output = 0;
-            output = output | ((uint64_t) sign << 63);
-            output = output | ((uint64_t) IEEEexponent << 52);
-            output = output | fraction >> 4;
+
+            if (IEEEexponent > 0x7FE)
+            {
+                output = output | ((uint64_t) sign << 63);
+                output = output | 0x7FF0000000000000;
+            }
+            else if (IEEEexponent < 0x1)
+            {
+                IEEEexponent = (-1 * IEEEexponent) + 1;
+
+                fraction = fraction | 0x10000000000000;
+                fraction = fraction >> IEEEexponent;
+
+                output = output | ((uint64_t) sign << 63);
+                output = output | fraction;
+            }
+            else
+            {
+                output = output | ((uint64_t) sign << 63);
+                output = output | ((uint64_t) IEEEexponent << 52);
+                output = output | fraction;
+            }
 
             //printf("%llx\n", output);
 
@@ -175,17 +199,16 @@ int main(int argc, char * argv[])
             uint16_t IBMexponent = (inputBuffer[0] & 0x7F);
             uint64_t fraction = ((uint64_t)inputBuffer[1] << 48) | ((uint64_t)inputBuffer[2] << 40) | (uint64_t)inputBuffer[3] << 32 | (uint64_t)inputBuffer[4] << 24 | (uint64_t)inputBuffer[5] << 16 | (uint64_t)inputBuffer[6] << 8 | (uint64_t)inputBuffer[7];
 
-            printf("fraction: %llx\n", fraction);
-            printf("exponent: %d\n", IBMexponent);
-            printf("sign: %x\n", sign);
+//            printf("fraction: %llx\n", fraction);
+//            printf("exponent: %d\n", IBMexponent);
+//            printf("sign: %x\n", sign);
 
             // find the index of the first 1 in the fraction
             uint16_t oneIndex;
             for (oneIndex = 0; oneIndex < 65; oneIndex++) {
                 if (oneIndex == 64)
                 {
-                    // TODO no ones found, = 0
-                    printf("todo\n");
+                    oneIndex = 56;
                     break;
                 }
                 if (fraction >> oneIndex == 1)
@@ -197,18 +220,47 @@ int main(int argc, char * argv[])
             //printf("%d\n", oneIndex);
 
             // move the fraction left by oneIndex for the implicit 1 of IEEE and cut off the first one
-            fraction = (fraction << oneIndex) & 0xFFFFFFFFFFFFFF;
+            fraction = (uint32_t)(((fraction << oneIndex) & 0xFFFFFFFFFFFFFF) >> 33);
 
             // new exponent (number of bits the fraction should be moved)
             // the number of bits IBM moved it
             // minus the index of the first one as IEEE has an implicit one
             // IEEE single is biased by 127
-            uint16_t IEEEexponent = ((IBMexponent - 64) * 4) - oneIndex + 127;
+            int32_t IEEEexponent = ((IBMexponent - 64) * 4) - oneIndex + 127;
 
             uint32_t output = 0;
-            output = output | ((uint32_t) sign << 31);
-            output = output | ((uint32_t) IEEEexponent << 23);
-            output = output | (uint32_t)(fraction >> 33);
+
+            // largest possible IEEE exponent for a normal value is 11111110 (254)
+            if (IEEEexponent > 0xFE)
+            {
+                //printf("infinity\n");
+                // (sign)1111111100000000000000000000000 is +/- infinity
+                output = output | ((uint32_t)sign << 31);
+                output = output | 0x7F800000;
+            }
+                // smallest possible IEEE exponent for a normal value is 1
+            else if (IEEEexponent < 0x1)
+            {
+                // move the fraction by number of bits the exponent would move it, then create the number
+                IEEEexponent = (-1 * IEEEexponent) + 1;
+
+                //printf("moving by: %d\n", IEEEexponent);
+
+                // re-add the implicit 1 to the front of the fraction
+                fraction = fraction | 0x800000;
+                fraction = fraction >> IEEEexponent;
+
+                // construct the output, exponent = 0
+                output = output | ((uint32_t)sign << 31);
+                output = output | (uint32_t)fraction;
+                //printf("output: %x\n", output);
+            }
+            else
+            {
+                output = output | ((uint32_t)sign << 31);
+                output = output | ((uint32_t)IEEEexponent << 23);
+                output = output | (uint32_t)fraction;
+            }
 
             //printf("%llx\n", output);
 
@@ -243,7 +295,7 @@ int main(int argc, char * argv[])
             {
                 if (oneIndex == 32)
                 {
-                    // TODO no ones found, = 0
+                    oneIndex = 24;
                     break;
                 }
                 if (fraction >> oneIndex == 1)
@@ -254,7 +306,7 @@ int main(int argc, char * argv[])
             }
 
             // move the fraction left by oneIndex for the implicit 1 of IEEE
-            fraction = (fraction << oneIndex) & 0xFFFFFF;
+            fraction = (((uint64_t)fraction << oneIndex) & 0xFFFFFF) << 28;
 
             // new exponent (number of bits the fraction should be moved)
             // the number of bits IBM would move the fraction
@@ -263,9 +315,28 @@ int main(int argc, char * argv[])
             uint16_t IEEEexponent = ((IBMexponent - 64) * 4) - oneIndex + 1023;
 
             uint64_t output = 0;
-            output = output | ((uint64_t)sign << 63);
-            output = output | ((uint64_t)IEEEexponent << 52);
-            output = output | (uint64_t)fraction << 28;
+
+            if (IEEEexponent > 0x7FE)
+            {
+                output = output | ((uint64_t) sign << 63);
+                output = output | 0x7FF0000000000000;
+            }
+            else if (IEEEexponent < 0x1)
+            {
+                IEEEexponent = (-1 * IEEEexponent) + 1;
+
+                fraction = fraction | 0x10000000000000;
+                fraction = fraction >> IEEEexponent;
+
+                output = output | ((uint64_t) sign << 63);
+                output = output | (uint64_t)fraction;
+            }
+            else
+            {
+                output = output | ((uint64_t) sign << 63);
+                output = output | ((uint64_t) IEEEexponent << 52);
+                output = output | (uint64_t)fraction;
+            }
 
             //printf("output: %x\n", output);
 
@@ -304,20 +375,22 @@ int main(int argc, char * argv[])
             {
                 if (oneIndex == 32)
                 {
-                    // TODO: no ones found, this is a zero
+                    oneIndex = 24;
+                    // this is a zero
                     break;
                 }
                 // shift the fraction right until it's only a single 1
                 if (fraction >> oneIndex == 1)
                 {
+                    // one index measures the distance of the first one from the right of the number
                     // set oneindex to be relative to the 24 bits of the fraction, not the whole number
                     oneIndex = (32 - oneIndex) - 8;
                     break;
                 }
             }
 
-            printf("one index: %d\n", oneIndex);
-            printf("ibm exponent: %d\n", IBMexponent);
+            //printf("one index: %d\n", oneIndex);
+            //printf("ibm exponent: %d\n", (IBMexponent - 64) * 4);
 
             // move the fraction left by oneIndex for the implicit 1 of IEEE
             // and & it with 24 bits of 1 to remove the implicit one
@@ -336,24 +409,23 @@ int main(int argc, char * argv[])
 
             uint32_t output = 0;
 
-            printf("ieee exponent: %d\n", IEEEexponent);
+            //printf("ieee exponent: %d\n", IEEEexponent - 127);
 
             // largest possible IEEE exponent for a normal value is 11111110 (254)
             if (IEEEexponent > 0xFE)
             {
-                printf("infinity\n");
-                output = output | ((uint32_t)sign << 31);
-
+                //printf("infinity\n");
                 // (sign)1111111100000000000000000000000 is +/- infinity
+                output = output | ((uint32_t)sign << 31);
                 output = output | 0x7F800000;
             }
             // smallest possible IEEE exponent for a normal value is 1
             else if (IEEEexponent < 0x1)
             {
-                // move the fraction by number of bits the exponent would move it, plus one as the fraction now contains the implicit one, then create the number
+                // move the fraction by number of bits the exponent would move it, then create the number
                 IEEEexponent = (-1 * IEEEexponent) + 1;
 
-                printf("moving by: %d\n", IEEEexponent);
+                //printf("moving by: %d\n", IEEEexponent);
 
                 // re-add the implicit 1 to the front of the fraction
                 fraction = fraction | 0x800000;
@@ -362,7 +434,7 @@ int main(int argc, char * argv[])
                 // construct the output, exponent = 0
                 output = output | ((uint32_t)sign << 31);
                 output = output | fraction;
-                printf("output: %x\n", output);
+                //printf("output: %x\n", output);
             }
             else
             {
@@ -373,7 +445,7 @@ int main(int argc, char * argv[])
 
             //printf("output: %x\n", output);
 
-            printIEEEsingle(output);
+            //printIEEEsingle(output);
 
             // write the bytes of output to the output file in the correct order
             for (int byteIndex = sizeof(output) - 1; byteIndex >= 0; byteIndex --)
--- README.txt
+++ README.txt
@@ -7,6 +7,12 @@ Instructions:
 	2. Run the program with: FloatingPoint
 	3. the program will ask for an input filename, its precision, and output filename and its precision, precision can either be single (S) or double (D)
 	
-Test cases:
+Test cases (IBM singles => IEEE singles):
 11000010011101101010000000000000 (-118.625)
-01000010011011001010110100010101 (108.676)
+01000010011011001010110100010101 (108.6761)
+01100000111111111111111111111111 (largest positive IEEE single)
+01100001111111111111111111111111 (largest positive IEEE single plus 1, (infinity))
+11100000111111111111111111111111 (largest negative IEEE single)
+00100001010000000000000000000000 (smallest normal IEEE single)
+00100001001111111111111111111111 (largest denormalised IEEE single (technically not quite but its as close as i can get))
+00011011100000000000000000000000 (smallest denormalised IEEE single)
